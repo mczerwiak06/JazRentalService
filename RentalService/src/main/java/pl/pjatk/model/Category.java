42c2706d2b17fa7065f5330017751323c2cc2cd4
Michał Czerwiak <michal.czerwiak@gmail.com>
2021-06-05 16:49:03 +0200

    added Movie class and BeanConfig

---
+++ RentalService/src/main/java/pl/pjatk/model/Category.java
@@ -0,0 +1,14 @@
+package pl.pjatk.model;
+
+public enum Category {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    FANTASY,
+    HORROR,
+    MYSTERY,
+    ROMANCE,
+    THRILLER,
+    WESTERN,
+    SCIFI
+}
---
+++ RentalService/src/main/java/pl/pjatk/model/Movie.java
@@ -0,0 +1,64 @@
+package pl.pjatk.model;
+
+public class Movie {
+   // @Id
+   // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    //@Enumerated(EnumType.STRING)
+    private Category category;
+    private byte isAvailable;
+
+    public byte getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(byte available) {
+        isAvailable = available;
+    }
+
+    public Movie(Long id, String name, Category category, byte isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.isAvailable = isAvailable;
+    }
+
+    public Movie(String name, Category category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public Movie(Long id, String name, Category category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+
+    public Movie() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
